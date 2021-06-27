f90fd2f7d47930427dc9da472eb788b33a37a28d
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-06-27 19:37:55 -0400

    got basic character component working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [baseURL] = useState("https://swapi.dev/api/people");
-  const [characters, setCharacters] = useState([])
+
+  const [character, setCharacter] = useState([])
+  // const [search, setSearch] = useState("")
 
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
@@ -15,17 +17,24 @@ const App = () => {
   // sync up with, if any.
 
   useEffect(() => {
-    axios.get(baseURL)
+    axios.get("https://swapi.dev/api/people")
       .then((res) => {
-        // to see object in console - can delete later
-        console.log(res.data.results)
-        setCharacters(res.data.results)
+        console.log(res.data)
+        setCharacter(res.data)
       })
+      .catch(error => { return error })
   }, [])
+
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Major Characters List</h1>
+
+      <main>
+        <Character myName="Chris" myAge="27" character={character} />
+
+      </main>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from "react"
+
+const Character = (props) => {
+
+    return (
+        <section>
+            {
+                props.character.map(character => (
+                    <div>
+                        <h3>{character.name} </h3>
+                        {/* <button onClick={close}></button> */}
+                        <p>gender: {character.gender}</p>
+                        <p>Height: {character.height}</p>
+                        <p>Mass: {character.mass}</p>
+                        <p>BirthYear: {character.birth_year}</p>
+                        <p>Eye Color: {character.eye_color}</p>
+                        <p>Hair Color: {character.hair_color}</p>
+                        <p>Skin Color: {character.skin_color}</p>
+                    </div>
+                ))
+            }
+        </section>
+    )
+}
+
+export default Character
